1661c63304d2ee28bf45c8e5cd4151fc62032f80
Dominik Curyło <curylo.dominik@gmail.com>
2012-05-23 19:10:58 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gargamel::Application.config.secret_token = 'd63452f833cdca2235adb32b0f4e237260660d2af0e5edc295c8ab48486a50d23278109da155f7ad204dc06219f62af28d092267b8c88f11090780e491f29a2f'
